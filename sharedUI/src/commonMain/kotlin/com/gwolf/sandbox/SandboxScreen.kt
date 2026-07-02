71c50c9c914c00c0457beff0da18bac50b76428b
Artem Naumov <artem.naumov.dev@gmail.com>
2026-07-02 02:04:17 +0200

    feat(ui): implement VSync-backed game loop using withFrameNanos

---
+++ sharedUI/src/commonMain/kotlin/com/gwolf/sandbox/SandboxScreen.kt
@@ -0,0 +1,29 @@
+package com.gwolf.sandbox
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.withFrameNanos
+import com.gwolf.sandbox.core.Engine
+import com.gwolf.sandbox.core.Grid
+import kotlinx.coroutines.isActive
+
+@Composable
+fun SandboxScreen() {
+
+    val grid = remember { Grid() }
+    val engine = remember { Engine(grid) }
+
+    LaunchedEffect(Unit) {
+        var previousTimeNanos = System.nanoTime()
+
+        while (isActive) {
+            withFrameNanos { frameTimeNanos ->
+                val deltaTime = (frameTimeNanos - previousTimeNanos) / 1_000_000_000f
+                previousTimeNanos = frameTimeNanos
+
+                engine.update(deltaTime)
+            }
+        }
+    }
+}
