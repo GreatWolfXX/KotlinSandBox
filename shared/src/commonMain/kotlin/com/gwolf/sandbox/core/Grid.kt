fcdab835397981652914d002d84f829c20e09049
Artem Naumov <artem.naumov.dev@gmail.com>
2026-07-02 02:27:53 +0200

    feat(core): introduce Element constants and update Grid initialization with mock data

--- shared/src/commonMain/kotlin/com/gwolf/sandbox/core/Grid.kt
+++ shared/src/commonMain/kotlin/com/gwolf/sandbox/core/Grid.kt
@@ -1,5 +1,7 @@
 package com.gwolf.sandbox.core
 
+import kotlin.random.Random
+
 class Grid : GridState {
 
     override val width: Int = GRID_WIDTH
@@ -29,5 +31,25 @@ class Grid : GridState {
 
     override fun inBounds(x: Int, y: Int): Boolean {
         return x in 0 until width && y in 0 until height
+    }
+
+    init {
+        for (x in 0 until width) {
+            setElement(x, height - 10, Element.WALL)
+        }
+
+        for (i in 0 until (width * height / 10)) { // заполняем ~10% пространства
+            val randomX = Random.nextInt(0, width)
+            val randomY = Random.nextInt(0, height - 20) // чуть выше платформы
+
+            setElement(randomX, randomY, Element.SAND)
+        }
+
+        val centerX = width / 2
+        val centerY = height / 2
+        for (i in -5..5) {
+            if (inBounds(centerX + i, centerY)) setElement(centerX + i, centerY, Element.WALL)
+            if (inBounds(centerX, centerY + i)) setElement(centerX, centerY + i, Element.SAND)
+        }
     }
 }
